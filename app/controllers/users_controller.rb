3dca090953a85ff7a2d450482274b3918a9e09d4
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2023-11-21 10:49:41 +1100

    edited users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,11 +12,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   private
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :password)
-    end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :password)
+  end
 end
