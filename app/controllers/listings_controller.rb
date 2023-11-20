07bca6ea9e072a24a43e5719ad925ae032e7d58f
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-20 16:03:49 +1100

    Refactored show with set_listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,34 @@
 class ListingsController < ApplicationController
   # Will need a before action I think, one to allow non-users to search still?
   # Do photos get added here? Sounds like a tomorrow job
+  before_action :set_listing, only: [:show]
 
   def index
     @listings = Listing.all
   end
 
   def show
-    @listing = Listing.find(params[:id])
+    set_listing
   end
 
+  def new
+    @listing.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def listing_params
+    # Protects against CRLF injection
+    # Need to pass photo later
+    params.require(:listing).permit(:title, :sport_type, :description, :hourly_price)
+  end
+
+  def set_listing
+    # Finds model per id
+    @listing = Listing.find(params[:id])
+  end
 end
