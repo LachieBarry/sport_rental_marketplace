76ab472c42b73a83dfe59adc800ddc22a098c420
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-21 17:01:35 +1100

    Bookings#create and new completed and tested - need to update form later

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new
+  end
+
+  def create
+    # STILL NEED TO IMPLEMENT FLATPICKR
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new(booking_params)
+    @booking.listing_id = @listing.id
+    @booking.user_id = current_user.id
+    @booking.total_price = ((@booking.end_date - @booking.start_date) / 3600) * @listing.hourly_price
+    @booking.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price, :listing_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  resources :listings
+  resources :listings do
+    resources :bookings, only: [:new, :create, :show]
+  end
   # resources :bookings will nest here
 end
