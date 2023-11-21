01c87a84fdb4872efdb2c4cd254ce5954bb73067
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-21 15:09:32 +1100

    accidentally made the model here - sorry

---
+++ db/migrate/20231121040812_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.references :user, null: false, foreign_key: true
+      t.references :listing, null: false, foreign_key: true
+      t.integer :accepted
+      t.string :total_price
+
+      t.timestamps
+    end
+  end
+end
