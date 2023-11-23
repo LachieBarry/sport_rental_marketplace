b2372ef170faa1c9f3d5324f0ed28246ed0262bc
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-23 11:46:49 +1100

    added validations to user - could generate accounts without a last_name and with number as a string (which I think would nil out)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :listings
 
+  validates :first_name, :last_name, :phone_number, presence: true
+  validates :phone_number, numericality: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
