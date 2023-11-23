f9342d42374de63a50b039e3a13a5262595d577b
hanh lockett <honghanhbr2007@yahoo.com>
2023-11-24 10:46:46 +1100

    home page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,9 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// commonjs
+const flatpickr = require("flatpickr");
+
+// es modules are recommended, if available, especially for typescript
+import flatpickr from "flatpickr";
