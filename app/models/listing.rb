b62927c43240036739c090f02944d4725074d146
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-23 11:35:39 +1100

    Fixed bug Hahn was having, was caused by there being no requirement for photo presence, this allowed you to make a listing with no photo - thus hitting an error with cloudinary. You would also hit an error making a listing without a photo, but it would still make the erronious instance

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,7 +10,7 @@ class Listing < ApplicationRecord
   # sports type
   valid_sports = ['AFL', 'Gridiron', 'Rugby', 'Netball', 'Cricket', 'Baseball', 'Basketball', 'Softball', 'Golf', 'Tennis', 'Frisbee', 'Volleyball', 'Cycling', 'BMX', 'Martial Arts', 'Gymnastics', 'Curling', 'Ice Skating', 'Ice Hockey', 'Field Hockey', 'Skateboarding', 'Snowboarding', 'Skiing', 'Surfing', 'Sledding', 'Rowing', 'Water Polo', 'Kickball', 'Dodgeball', 'Lacrosse', 'Rollerskating', 'Handball', 'Dodgeball', 'Hurling', 'Polo', 'Badmitton', 'Squash', 'Table Tennis', 'Other']
   # validations
-  validates :title, :description, :sport_type, :hourly_price, presence: true
+  validates :title, :description, :sport_type, :hourly_price, :photo, presence: true
   validates :title, length: { minimum: 2 }
   validates :description, length: { maximum: 300 }
   validates :hourly_price, numericality: true
