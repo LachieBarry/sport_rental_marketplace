bcd965f5904cbff29ace225079ac8dbf520bb9d4
Lachlan Barry <lachlan.j.barry@gmail.com>
2023-11-24 16:49:13 +1100

    last check before demo, fixed some links, changes some cslight stylings and confirmed connectivity

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,7 +33,7 @@ class BookingsController < ApplicationController
     @booking.user_id = current_user.id
     @booking.total_price = ((@booking.end_date - @booking.start_date) / 3600) * @listing.hourly_price
     @booking.save
-    redirect_to listing_path(@listing)
+    redirect_to listings_path
   end
 
   def show
